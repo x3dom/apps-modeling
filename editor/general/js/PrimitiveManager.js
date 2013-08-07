c8589de1164c7e1c7f02e4de864acdd19065cbdc
Michael Englert <michael.englert@igd.fraunhofer.de>
2013-08-07 23:36:32 +0200

    Transformation by field value

--- editor/general/js/PrimitiveManager.js
+++ editor/general/js/PrimitiveManager.js
@@ -3,8 +3,8 @@
  * added primitives of the workspace
  * @returns {PrimitiveManager}
  */
-function PrimitiveManager()
-{    
+function PrimitiveManager(){
+    
     // Counter to create unique identificators
     var shapeCounter = 0;
     // List of all created primitives
@@ -18,14 +18,13 @@ function PrimitiveManager()
      * @param {type} primitive name of the primitive that should be created
      * @returns {Boolean}
      */
-    this.addPrimitive = function(primitive)
-    {
-    
-        actualID = "element_" + shapeCounter;
+    this.addPrimitive = function(primitive){
+   
+        var id = "element_" + shapeCounter;
         shapeCounter++;
 
         var t = document.createElement('Transform');
-        t.setAttribute("id", actualID);
+        t.setAttribute("id", id);
         t.setAttribute("translation", "0 0 0");
         var s = document.createElement('Shape');
 
@@ -47,15 +46,17 @@ function PrimitiveManager()
 
         var ot = document.getElementById('root');
         ot.appendChild(t);
-
-        document.getElementById(actualID).addEventListener("click", function(){primitiveSelected(actualID)}, false);
+        
+        primitiveList[id] = t;
+        primitiveList[id].addEventListener("click", function(){primitiveSelected(id);}, false);
         
         var trans = (Math.random() * 100 % 5) + " " + (Math.random() * 100 % 5) + " " + (Math.random() * 100 % 5);
-        document.getElementById(actualID).setAttribute("translation", trans);
-        setTranslationValues(actualID, HANDLING_MODE);
+        primitiveList[id].setAttribute("translation", trans);
+        setTransformValues(id, HANDLING_MODE);
         
-        primitiveList[actualID] = t;
+        actualID = id;
     };
+    
     
     
     /*
@@ -63,50 +64,97 @@ function PrimitiveManager()
      * TODO: Das Element muss auch aus der primitiveList[] entfernt werden
      * Remove Node
      */     
-	this.removeNode = function()
-	{
-		var ot = document.getElementById(actualID);
-		
-		for (var i = 0; i < ot.childNodes.length; i++) 
-		{
-			// check if we have a real X3DOM Node; not just e.g. a Text-tag
-	        if (ot.childNodes[i].nodeType === Node.ELEMENT_NODE) 
-	        {
-	        	ot.removeChild(ot.childNodes[i]);
-	  			break;
-	  		}
-	  	}
-	};
+    this.removeNode = function()
+    {
+        var ot = document.getElementById(actualID);
+
+        for (var i = 0; i < ot.childNodes.length; i++) 
+        {
+            // check if we have a real X3DOM Node; not just e.g. a Text-tag
+            if (ot.childNodes[i].nodeType === Node.ELEMENT_NODE) 
+            {
+                ot.removeChild(ot.childNodes[i]);
+                    break;
+            }
+        }
+    };
+
+    
     
     
     /*
      * Will be called if a primitive is selected and should
      * set the values of translation, rotation or scaling
+     * @param {type} id name of the primitive's values that should be set
+     * @returns {null}
      */
     function primitiveSelected(id)
     {
-        setTranslationValues(id, HANDLING_MODE);
-        alert(primitiveList[id].children.shape.children[1].attributes.length);
         actualID = id;
+        setTransformValues(id, HANDLING_MODE);
     }
     
     
-    this.setValues = function(){
-        setTranslationValues(actualID, HANDLING_MODE);
+    
+    /*
+     * Sets the values of the actual selected transformation
+     * to the value fields in the bottom bar
+     * @returns {null}
+     */
+    this.setTransformationValues = function(){
+        setTransformValues(actualID, HANDLING_MODE);
     };
     
     
+    
     /*
-     * Sets the translation
+     * 
+     * @returns {undefined}
      */
-    function setTranslationValues(id, mode)
-    {
-        var shape = document.getElementById(id);
-        var xyz; (mode === "translation") ? xyz = shape.attributes[mode].nodeValue.split(" ") : 
-                                            xyz = shape.attributes[mode].nodeValue.split(",");
+    this.setTransformationValuesToPrimitive = function() {
+        var tempValue = ""; 
+
+        if(HANDLING_MODE === "translation") {
+            tempValue = document.getElementById("amount-x").value + " " +
+                        document.getElementById("amount-y").value + " " +
+                        document.getElementById("amount-z").value;
+            primitiveList[actualID].setAttribute(HANDLING_MODE, tempValue);
+        }
+        else if (HANDLING_MODE === "rotation") {
+            primitiveList[actualID].setAttribute(HANDLING_MODE, "1,0,0," + (document.getElementById("amount-x").value * (Math.PI / 180.0)));
+            //primitiveList[actualID].setAttribute(HANDLING_MODE, "0,1,0," + (document.getElementById("amount-y").value * (Math.PI / 180.0)));
+            //primitiveList[actualID].setAttribute(HANDLING_MODE, "0,0,1," + (document.getElementById("amount-z").value * (Math.PI / 180.0)));
+        }
+        else {
+            tempValue = document.getElementById("amount-x").value + "," +
+                        document.getElementById("amount-y").value + "," +
+                        document.getElementById("amount-z").value;
+            primitiveList[actualID].setAttribute(HANDLING_MODE, tempValue);
+        }
+        
+        
+    };
+    
+    
+    
+    
+    /*
+     * Sets the values of the actual selected transformation
+     * to the value fields in the bottom bar
+     * @param {type} id name of the primitive's values that should be set
+     * @param {type} mode type of transformation 
+     * @returns {null}
+     */
+    function setTransformValues(id, mode){
+        var xyz = ""; 
+        
+        (mode === "translation") ? xyz = primitiveList[id].attributes[mode].nodeValue.split(" ") : 
+                                   xyz = primitiveList[id].attributes[mode].nodeValue.split(",");
         
         document.getElementById("amount-x").value = xyz[0].substr(0, 5);
         document.getElementById("amount-y").value = xyz[1].substr(0, 5);
         document.getElementById("amount-z").value = xyz[2].substr(0, 5);
+        
+        document.getElementById("ObjektName").value = id;
     }
 }
