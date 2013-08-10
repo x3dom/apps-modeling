24062991c1825a0010d09158514c6da7e6c86860
Andres Kordek <andres.kordek@gmail.com>
2013-08-10 04:29:04 +0200

    ViewPoints added.

--- editor/general/js/main.js
+++ editor/general/js/main.js
@@ -94,6 +94,56 @@ function someDiv()
 }
 
 
+/*
+ * Set Viewpoint
+ */
+function setViewpoint(point)
+{	
+	if(point == "none")
+	{
+		var vPoint = document.getElementById("viewPoint");
+		vPoint.setAttribute("position", "9.89187 11.41910 20.11090");
+		vPoint.setAttribute("orientation", "-0.69262 0.71082 0.12256 0.61209");
+	}
+	else if(point == "front")
+	{
+		var vPoint = document.getElementById("viewPoint");
+		vPoint.setAttribute("position", "0 0 30");
+		vPoint.setAttribute("orientation", "0 0 0 0");
+	}
+	else if(point == "back")
+	{
+		var vPoint = document.getElementById("viewPoint");
+		vPoint.setAttribute("position", "0 0 -30");
+		vPoint.setAttribute("orientation", "0 1 0 3.142");
+	}
+	else if(point == "right")
+	{
+		var vPoint = document.getElementById("viewPoint");
+		vPoint.setAttribute("position", "30 0 0");
+		vPoint.setAttribute("orientation", "0 1 0 1.571");
+	}
+	else if(point == "left")
+	{
+		var vPoint = document.getElementById("viewPoint");
+		vPoint.setAttribute("position", "-30 0 0");
+		vPoint.setAttribute("orientation", "0 1 0 -1.571");
+	}
+	else if(point == "top")
+	{
+		var vPoint = document.getElementById("viewPoint");
+		vPoint.setAttribute("position", "0 30 0");
+		vPoint.setAttribute("orientation", "1 0 0 -1.571");
+		
+	}
+	else if(point == "bottom")
+	{		
+		var vPoint = document.getElementById("viewPoint");
+		vPoint.setAttribute("position", "0 -30 0");
+		vPoint.setAttribute("orientation", "1 0 0 1.571");
+	}		
+}
+
 
 /*
  * Initialization of all JQuery elements
