b05de0d6a2c1a4f315350e475cc00654648bf808
Andres Kordek <andres.kordek@gmail.com>
2013-08-13 05:03:47 +0200

    Changes the plane in the views.It rotates with the axis.

--- editor/general/js/main.js
+++ editor/general/js/main.js
@@ -104,30 +104,45 @@ function setViewpoint(point)
 		var vPoint = document.getElementById("viewPoint");
 		vPoint.setAttribute("position", "9.89187 11.41910 20.11090");
 		vPoint.setAttribute("orientation", "-0.69262 0.71082 0.12256 0.61209");
+		
+		var vPlane = document.getElementById("planeId");
+		vPlane.setAttribute("rotation", "1 0 0 1.571");
 	}
 	else if(point == "front")
 	{
 		var vPoint = document.getElementById("viewPoint");
 		vPoint.setAttribute("position", "0 0 30");
 		vPoint.setAttribute("orientation", "0 0 0 0");
+		
+		var vPlane = document.getElementById("planeId");
+		vPlane.setAttribute("rotation", "1 0 0 0");
 	}
 	else if(point == "back")
 	{
 		var vPoint = document.getElementById("viewPoint");
 		vPoint.setAttribute("position", "0 0 -30");
 		vPoint.setAttribute("orientation", "0 1 0 3.142");
+		
+		var vPlane = document.getElementById("planeId");
+		vPlane.setAttribute("rotation", "1 0 0 0");
 	}
 	else if(point == "right")
 	{
 		var vPoint = document.getElementById("viewPoint");
 		vPoint.setAttribute("position", "30 0 0");
 		vPoint.setAttribute("orientation", "0 1 0 1.571");
+		
+		var vPlane = document.getElementById("planeId");
+		vPlane.setAttribute("rotation", "0 1 0 1.571");
 	}
 	else if(point == "left")
 	{
 		var vPoint = document.getElementById("viewPoint");
 		vPoint.setAttribute("position", "-30 0 0");
 		vPoint.setAttribute("orientation", "0 1 0 -1.571");
+		
+		var vPlane = document.getElementById("planeId");
+		vPlane.setAttribute("rotation", "0 1 0 1.571");
 	}
 	else if(point == "top")
 	{
@@ -135,12 +150,17 @@ function setViewpoint(point)
 		vPoint.setAttribute("position", "0 30 0");
 		vPoint.setAttribute("orientation", "1 0 0 -1.571");
 		
+		var vPlane = document.getElementById("planeId");
+		vPlane.setAttribute("rotation", "1 0 0 1.571");
 	}
 	else if(point == "bottom")
 	{		
 		var vPoint = document.getElementById("viewPoint");
 		vPoint.setAttribute("position", "0 -30 0");
 		vPoint.setAttribute("orientation", "1 0 0 1.571");
+		
+		var vPlane = document.getElementById("planeId");
+		vPlane.setAttribute("rotation", "1 0 0 1.571");
 	}		
 }
 
