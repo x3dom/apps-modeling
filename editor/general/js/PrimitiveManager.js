89cd971977e106f794d1e2ccf986795142e88ca8
Michael Englert <michael.englert@igd.fraunhofer.de>
2013-08-13 11:32:20 +0200

    code refactoring

--- editor/general/js/PrimitiveManager.js
+++ editor/general/js/PrimitiveManager.js
@@ -302,6 +302,11 @@ function PrimitiveManager(){
 
 function Controller(){
     
+    /*
+     * Activates the specified transformation mode 
+     * @param {string} mode transformation mode (translation, scale, rotation, hand)
+     * @returns {Null}
+     */
     this.Activate = function(mode){
         
         HANDLING_MODE = mode;
@@ -336,5 +341,79 @@ function Controller(){
             document.getElementById("primitiveList").selectedIndex = 0;
             document.getElementById("primitiveList").disabled = true;
         }
+    };
+    
+    
+    
+    /*
+     * Sets the specified view point in the editor
+     * @param {string} viewpoint name of the viewpoint that should be displayed
+     * @returns {Null}
+     */
+    this.setViewpoint = function(point)
+    {	
+        if(point === "none")
+        {
+            var vPoint = document.getElementById("viewPoint");
+            vPoint.setAttribute("position", "9.89187 11.41910 20.11090");
+            vPoint.setAttribute("orientation", "-0.69262 0.71082 0.12256 0.61209");
+
+            var vPlane = document.getElementById("planeId");
+            vPlane.setAttribute("rotation", "1 0 0 1.571");
+        }
+        else if(point === "front")
+        {
+            var vPoint = document.getElementById("viewPoint");
+            vPoint.setAttribute("position", "0 0 30");
+            vPoint.setAttribute("orientation", "0 0 0 0");
+
+            var vPlane = document.getElementById("planeId");
+            vPlane.setAttribute("rotation", "1 0 0 0");
+        }
+        else if(point === "back")
+        {
+            var vPoint = document.getElementById("viewPoint");
+            vPoint.setAttribute("position", "0 0 -30");
+            vPoint.setAttribute("orientation", "0 1 0 3.142");
+
+            var vPlane = document.getElementById("planeId");
+            vPlane.setAttribute("rotation", "1 0 0 0");
+        }
+        else if(point === "right")
+        {
+            var vPoint = document.getElementById("viewPoint");
+            vPoint.setAttribute("position", "30 0 0");
+            vPoint.setAttribute("orientation", "0 1 0 1.571");
+
+            var vPlane = document.getElementById("planeId");
+            vPlane.setAttribute("rotation", "0 1 0 1.571");
+        }
+        else if(point === "left")
+        {
+            var vPoint = document.getElementById("viewPoint");
+            vPoint.setAttribute("position", "-30 0 0");
+            vPoint.setAttribute("orientation", "0 1 0 -1.571");
+
+            var vPlane = document.getElementById("planeId");
+            vPlane.setAttribute("rotation", "0 1 0 1.571");
+        }
+        else if(point === "top")
+        {
+            var vPoint = document.getElementById("viewPoint");
+            vPoint.setAttribute("position", "0 30 0");
+            vPoint.setAttribute("orientation", "1 0 0 -1.571");
+
+            var vPlane = document.getElementById("planeId");
+            vPlane.setAttribute("rotation", "1 0 0 1.571");
+        }
+        else if(point === "bottom")
+        {		
+            var vPoint = document.getElementById("viewPoint");
+            vPoint.setAttribute("position", "0 -30 0");
+            vPoint.setAttribute("orientation", "1 0 0 1.571");
+
+            var vPlane = document.getElementById("planeId");
+            vPlane.setAttribute("rotation", "1 0 0 1.571");
+        }		
     };
 }
