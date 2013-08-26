dc43db756f7cc10cc9a1d6776df08785cbcc069c
Andres Kordek <andres.kordek@gmail.com>
2013-08-26 20:56:21 +0200

    Test update

--- testing/Snapping/js/Bouding.js
+++ testing/Snapping/js/Bouding.js
@@ -3,33 +3,39 @@
  */
 function Bouding()
 {
-	var center;
-	var radius;
+	var snapPoint;
+	var JsonPoints = [];
 	
 	this.init = function()
 	{
 		var selectID = [];
+		var pfad = './x3d/JsonFiles/Box.json';
 		
 		selectID = primitiveManager.getIDList();
 		
 		for(var i = 0; i < selectID.length; i++)
 		{
-			boundingPoint(selectID[i]);
+			/*
+			 * JSON gibt mir die Punktkoordinaten
+			 * in jeder diese Koordinaten erzeuge ich
+			 * mit boundingPoint eine Bindungskugel
+			 */
+			loadJSON(selectID[i], pfad);
 		}
 	};
 	
 	
-    function boundingPoint(id)
-    {
+    function boundingPoint(id, pfad, position)
+    {    	
     	var transform = document.createElement('Transform');
     	var transform_S = document.createElement('Shape');
     	var transform_S_A = document.createElement('Appearance');
-    	var transform_S_A_M = document.createElement('Material');
+    	var transform_S_A_M = document.createElement('Material');    	
     	var transform_S_A_M_S = document.createElement('Sphere');
     	
     	transform_S_A_M_S.setAttribute('radius', '0.1');
     	transform_S_A_M.setAttribute('diffuseColor', '#3FFFFF');
-    	transform.setAttribute('translation', '0 0 1.5');
+    	transform.setAttribute('translation', position);
     	transform.setAttribute('id', 'boundingPoint');
     	
     	transform_S_A.appendChild( transform_S_A_M );
@@ -39,20 +45,28 @@ function Bouding()
     	
     	var element = document.getElementById(id);
     	element.appendChild(transform);
-    	
-    	loadJSON();
     };
     
     
-	function loadJSON()
+	function loadJSON(id, pfad)
     {
 	    // json-string laden
-		var json = GetHttpText("./x3d/TestData/SimpleTube.json");
+		var json = GetHttpText(pfad);
 		
 		// aus dem string ein Array bilden
-		var jsonObj = JSON.parse(json);
+		var jsonObj = eval ('(' + json + ')');
+		
+		// die Arrays koennen dann wie folgt aufgerufen werden points[0]
+		var points = jsonObj.snapPoints;		
+		
+		for(var i = 0; i < points.length; i++)
+		{
+			//Zeugen die bounding Points
+			boundingPoint(id, pfad, points[i].toString());	
+		}
 		
 		console.log(jsonObj);
+		console.log(points[1]);
     };
 
 	
