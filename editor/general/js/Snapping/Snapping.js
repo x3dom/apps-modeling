dbcc321f8e54cc1fb537a531eda403785e68f700
Andres Kordek <andres.kordek@gmail.com>
2013-09-01 22:28:11 +0200

    Snapping Test Update

--- editor/general/js/Snapping/Snapping.js
+++ editor/general/js/Snapping/Snapping.js
@@ -8,7 +8,9 @@ function Snapping()
 		setSnapping();
 	};
 	
-    // Snapping temporary test
+    /*
+     * Observers properties are added to any existing element
+     */
     function setSnapping()
     {
     	elementList = [];
@@ -17,28 +19,34 @@ function Snapping()
     	var snapObserver = new SnapObserver();
     	var snapSubject = new SnapSubject();
     	
-	    if(primitiveManager.getIDList().length != null)
+	    if(elementList.length != null)
 	    {
 	    	for(var i = 0; i < elementList.length; i++)
 	    	{
 	    		element = document.getElementById(elementList[i]);	
-	    		
 	    		console.log(elementList[i]);
-	    		
-		    	//Subject is observed
-		        SnapInherits(snapSubject, element);       
-				element["onclick"] = new Function("element.Report('elementList[i]')");
-					 		
+	    		 		
+	    		//Subject is observed
+		        SnapInherits(snapSubject, element);
+		        //Added onclick      
+				element["onclick"] = new Function('element.Report(snapping.getParameter())');
+				
+				
 		 		//Observer what makes Subject
-				SnapInherits(snapObserver, element);
-		        element.Update = function( param )
-		        {
-		        	console.log(param);
-		        };
-		        
+				SnapInherits(snapObserver, element); 				
+		        //Updates the changed parameters
+    	    	element.Update = function( param ){ console.log(param); };
 		        //Added to Observer list 
 		        element.AddObserver(element);
 	    	}
 	    }
+    };
+    
+    /*
+     * Each element calls this method when it is clicked, and is registered by the other elements
+     */
+    this.getParameter = function()
+    {
+    	return primitiveManager.getPosition( primitiveManager.getCurrentPrimitiveID() );
     };
 }
